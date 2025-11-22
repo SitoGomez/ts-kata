f0106257bce89d7191b9cfe25de2bf4b08dc4a8c
JoseGomezSevillano <jose.gomez@qualifyze.com>
2025-11-22 19:28:15 +0100

    feat: two and three

--- src/6_PrimeFactors/kata.spec.ts
+++ src/6_PrimeFactors/kata.spec.ts
@@ -1,21 +1,33 @@
 import { PrimeFactorsCalculator } from './kata';
 
 describe('Given an integer to get its prime factors', () => {
-  describe('When the integer is 1', () => {
-    it('Then the result should be an empty array', () => {
-      const primeFactorsCalculator = new PrimeFactorsCalculator();
-
-      const result = primeFactorsCalculator.getFrom(1);
-
-      expect(result).toEqual([]);
-    });
-  });
-
   describe('When the number is not an integer', () => {
     it('Then the result should fail', () => {
       const primeFactorsCalculator = new PrimeFactorsCalculator();
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       expect(() => primeFactorsCalculator.getFrom(1.5)).toThrow('Input must be a positive integer');
+    });
+  });
+
+  describe.each([
+    {
+      inputInteger: 1,
+      expectedResult: '[]',
+    },
+    {
+      inputInteger: 2,
+      expectedResult: '[2]',
+    },
+    {
+      inputInteger: 3,
+      expectedResult: '[3]',
+    },
+  ])('When the integer is $inputInteger', ({ inputInteger, expectedResult }) => {
+    it(`Then the result should be ${expectedResult}`, () => {
+      const primeFactorsCalculator = new PrimeFactorsCalculator();
+
+      const result = primeFactorsCalculator.getFrom(inputInteger);
+      expect(result).toBe(expectedResult);
     });
   });
 });
--- src/6_PrimeFactors/kata.ts
+++ src/6_PrimeFactors/kata.ts
@@ -1,10 +1,13 @@
 export class PrimeFactorsCalculator {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  public getFrom(inputNumber: number): any[] {
+  public getFrom(inputNumber: number): string {
     if (!Number.isInteger(inputNumber)) {
       throw new Error('Input must be a positive integer');
     }
 
-    return [];
+    if (inputNumber > 1) {
+      return '[' + inputNumber + ']';
+    }
+
+    return '[' + ']';
   }
 }
